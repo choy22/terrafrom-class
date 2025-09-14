f883aebdd79fdc9079985b6aec8fdf230b114faf
choy22 <57357038+choy22@users.noreply.github.com>
2025-09-14 09:04:17 +0900

    Update 02.vpc.tf

--- 02.vpc.tf
+++ 02.vpc.tf
@@ -1,8 +1,15 @@
-resource "aws_vpc" "terraform-vpc" {
-  cidr_block           = "10.250.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    "Name" = "terraform-vpc"
+terraform {
+  required_version = ">= 1.0.0, <2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "ap-northeast-2" #Asia Pacific (seoul) region
+
 }
